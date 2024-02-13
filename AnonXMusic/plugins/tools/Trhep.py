b380c2b34ed3260de7f5e22690fcbd072446f153
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-02-13 17:57:15 +0300

    Update Trhep.py

--- AnonXMusic/plugins/tools/Trhep.py
+++ AnonXMusic/plugins/tools/Trhep.py
@@ -6,7 +6,7 @@ from AnonXMusic import app
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = 5866649827 # حط ايديك هنا
+    dev_id = 117913435 # حط ايديك هنا
     if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
         info = await app.get_chat(dev_id)
         name = info.first_name
@@ -20,5 +20,5 @@ async def WelcomeDev(_, response: ChatMemberUpdated):
             chat_id=response.chat.id,
             reply_markup=markup,
             photo="downloads/developer.jpg", 
-            caption=f"‹ المطور دخل للكروب : {name} \n- {bio}"
+            caption=f"‹ WelCoM Dev. : {name} \n- {bio}"
         )
