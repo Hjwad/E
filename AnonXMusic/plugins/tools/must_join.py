cb76a5d5ea60518e6aaeb86fecca3793c86ceba4
Hjwad <158073869+Hjwad@users.noreply.github.com>
2024-02-13 17:51:05 +0300

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,27 +5,27 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/cczza":  # Not compulsory
+    if not "https://t.me/mmmsc":  # Not compulsory
         return
     try:
         try:
             await bot.get_chat_member("cczza", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/cczza".isalpha():
-                link = "https://t.me/cczza"
+            if "https://t.me/mmmsc".isalpha():
+                link = "https://t.me/mmmsc"
             else:
-                chat_info = await bot.get_chat("cczza")
+                chat_info = await bot.get_chat("mmmsc")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @cczza .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @mmmsc .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("فَلسفة مشاعر .", url=link)]
+                        [InlineKeyboardButton("Not sos.", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat @cczza !")
+        print(f"I'm not admin in the MUST_JOIN chat @mmmsc !")
